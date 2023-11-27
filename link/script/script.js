158d9140c6b32df17f5cac362e995663384dba2b
Denis <denis.d88@bk.ru>
2023-11-28 00:14:17 +0700

    В header добавлены индикаторы слайдера, в js - script переключения слайдера и индикаторов

--- link/script/script.js
+++ link/script/script.js
@@ -9,3 +9,21 @@ $(".list_of_goods").click(
 	}
 )
 
+/*Автоматический слайдер в шапке сайта на 3 фотографии - переключение каждые 5 сек */
+let sliderList = [
+	'./link/img/header/slider_header/first_slide.png',
+	'./link/img/header/slider_header/second_slide.png',
+	'./link/img/header/slider_header/thirt_slide.png'
+]
+
+let sliderCounter = 0;
+
+const intervalId = setInterval(function changeSliderPhoto() {
+	if (sliderCounter >= -1 && sliderCounter < 2) {
+		sliderCounter += 1;
+		$('.header_slider_img').attr("src", sliderList[sliderCounter]);
+		$(`.slider_indicator:eq(${sliderCounter})`).toggleClass('slider_indicator_orange');
+		$(`.slider_indicator:not(:eq(${sliderCounter}))`).removeClass('slider_indicator_orange');
+	}
+	else return sliderCounter = -1;
+}, 5000)
